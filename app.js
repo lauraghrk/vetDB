35b49468f9c3ab761ee2780784165507365b1d66
Laura Gehrke <76006992+lauraghrk@users.noreply.github.com>
2023-05-02 17:22:13 -0300

    add rout by id

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var registerRouter = require('./routes/register');
 
 var app = express();
 
@@ -20,7 +19,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/register', registerRouter);
+app.use('/register', indexRouter);
+app.use('/:_id', indexRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,27 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db')
 
+async function selectId(id) {
+  const list = await db.listAll()
+  return list.filter( pet => pet._id == id)
+}
+
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   const list = await db.listAll()
   res.render('index', { list });
 });
 
-router.post("/register", async (req, res) => {
+router.get('/:_id', async (req, res) => {
+  const json = selectId(req.params.id)
+  res.render('pet')
+})
+
+router.get('/register', async (req, res, next) => {
+  res.render('register');
+});
+
+router.post('/register', async (req, res) => {
   const pet = req.body
   const result = await db.register(pet)
   console.log(result)
